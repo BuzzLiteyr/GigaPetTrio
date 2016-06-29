0ff7db793bc6acfdcc33b8b8bab2d275bb85eb82
Michel Besnard <macmyjl@michel-besnards-macbook.local>
2016-06-29 19:22:32 -0300

    Initial Commit

---
+++ S4L57GigaPetTrio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S4L57GigaPetTrio
+//
+//  Created by Michel Besnard on 29-06-16.
+//  Copyright © 2016 Michel YJL Besnard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
